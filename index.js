193630699fbf96a9063737ba379f81ad07976d6b
Dominic Brant <dombrant@Dominics-MBP.fios-router.home>
2018-01-05 21:38:32 -0800

    added handling for options

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const random = require('./app.js');
+let max;
+let min;
 
-console.log(random(10, 0));
+if (process.argv.length == 2) {
+  max = 10;
+  min = 0;
+}
+
+if (process.argv.length == 3) {
+  max = Number(process.argv[2]);
+  min = 0;
+}
+
+if (process.argv.length == 4) {
+  max = Number(process.argv[2]);
+  min = Number(process.argv[3]);
+}
+
+const random = () => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(random());
