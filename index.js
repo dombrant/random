25335296362c8904018290d0ac92423c7eff039b
Dominic Brant <dombrant@Dominics-MacBook-Pro.local>
2018-01-06 19:53:24 -0800

    error handling if more than 2 arguments are passed

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ if (process.argv.length == 4) {
   min = Number(process.argv[3]);
 }
 
+else if (process.argv.length > 4) {
+  return console.log('ERROR: Please provide between 0 and 2 arguments');
+}
+
 const random = () => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
