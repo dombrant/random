ffb19dbe05d62418e7a365c874b46d006e831302
Dominic Brant <dombrant@gmail.com>
2018-05-06 18:35:36 -0700

    added export of random function for use in test.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ if (process.argv.length == 3) {
 if (process.argv.length == 4) {
   max = Number(process.argv[2]);
   min = Number(process.argv[3]);
-}
-
-else if (process.argv.length > 4) {
+} else if (process.argv.length > 4) {
   return console.log('ERROR: Please provide between 0 and 2 arguments');
 }
 
 const random = () => Math.floor(Math.random() * (max - min + 1) + min);
 
 console.log(random());
+
+module.exports = random;
