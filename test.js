6c638e1cfacf8d0ff6836953746f3dc204744d73
Dominic Brant <dombrant@gmail.com>
2018-05-06 18:37:30 -0700

    refactored test.js

--- test.js
+++ test.js
@@ -1,40 +1,38 @@
-const random = require('./app.js');
+const random = require('./index');
 
 let iterator = 0;
 const max = 10;
 const min = 0;
-const occurrences = [];
+const values = [];
 
 while (iterator < 11) {
   let randomValue = random(max, min);
 
-  if (!occurrences.includes(randomValue)) {
-    occurrences.push(randomValue);
+  if (!values.includes(randomValue)) {
+    values.push(randomValue);
     iterator++;
   }
 }
 // Populate the array with every number between max and min once
 
-
 const checkArray = () => {
   return new Promise((resolve, reject) => {
-    for (let i = 0; i < 11; i++) {
-      if (!occurrences.includes(i)) {
-        reject(`ERROR: array of random numbers between ${max} and ${min} does not include ${i}`);
+    values.forEach(number => {
+      if (!values.includes(number)) {
+        reject(
+          `ERROR: array of random numbers between ${max} and ${min} does not include ${number}`
+        );
       }
-    }
-    resolve(`Test passed for an array of random numbers between ${max} and ${min}`);
+    });
+    resolve(
+      `Test passed for an array of random numbers between ${max} and ${min}`
+    );
   });
-}
+};
 // Run a test on the array occurrences to check it contains every number between max and min,
 // and return a promise
 
-
 checkArray()
-  .then((resolve) => {
-    console.log(resolve);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-  // Log to the console the results of checkArray
+  .then(resolve => console.log(resolve))
+  .catch(error => console.log(error));
+// Log to the console the results of checkArray
