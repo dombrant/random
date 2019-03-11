d8b91564ae1d6a812924f83df065187e37ff154f
Dominic Brant <dombrant@gmail.com>
2019-03-11 10:01:24 -0700

    reformatted indentation

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ let max;
 let min;
 
 if (process.argv.length == 2) {
-  max = 10;
-  min = 0;
+    max = 10;
+    min = 0;
 }
 
 if (process.argv.length == 3) {
-  max = parseInt(process.argv[2]);
-  min = 0;
+    max = parseInt(process.argv[2]);
+    min = 0;
 }
 
 if (process.argv.length == 4) {
-  max = parseInt(process.argv[2]);
-  min = parseInt(process.argv[3]);
+    max = parseInt(process.argv[2]);
+    min = parseInt(process.argv[3]);
 } else if (process.argv.length > 4) {
-  return console.log('ERROR: Please provide between 0 and 2 arguments');
+    return console.log("ERROR: Please provide between 0 and 2 arguments");
 }
 
 const random = () => Math.floor(Math.random() * (max - min + 1) + min);
