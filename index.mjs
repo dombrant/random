b7b0714f498427006c6648a2d97ad819388af62b
Dominic Brant <dombrant@gmail.com>
2020-05-01 11:57:49 -0700

    updated to use es module syntax

--- index.mjs
+++ index.mjs
@@ -4,24 +4,24 @@ let max;
 let min;
 
 if (process.argv.length == 2) {
-    max = 10;
-    min = 0;
+  max = 10;
+  min = 0;
 }
 
 if (process.argv.length == 3) {
-    max = parseInt(process.argv[2]);
-    min = 0;
+  max = parseInt(process.argv[2]);
+  min = 0;
 }
 
 if (process.argv.length == 4) {
-    max = parseInt(process.argv[2]);
-    min = parseInt(process.argv[3]);
+  max = parseInt(process.argv[2]);
+  min = parseInt(process.argv[3]);
 } else if (process.argv.length > 4) {
-    return console.log("ERROR: Please provide between 0 and 2 arguments");
+  console.log("ERROR: Please provide between 0 and 2 arguments");
 }
 
 const random = () => Math.floor(Math.random() * (max - min + 1) + min);
 
 console.log(random());
 
-module.exports = random;
+export default random;
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const random = require("./index");
+import random from "./index.mjs";
 
 let iterator = 0;
 const max = 10;
@@ -6,33 +6,33 @@ const min = 0;
 const values = [];
 
 while (iterator < 11) {
-    let randomValue = random(max, min);
+  const randomValue = random(max, min);
 
-    if (!values.includes(randomValue)) {
-        values.push(randomValue);
-        iterator++;
-    }
+  if (!values.includes(randomValue)) {
+    values.push(randomValue);
+    iterator++;
+  }
 }
 // Populate the array with every number between max and min once
 
 const checkArray = () => {
-    return new Promise((resolve, reject) => {
-        values.forEach(number => {
-            if (!values.includes(number)) {
-                reject(
-                    `ERROR: array of random numbers between ${max} and ${min} does not include ${number}`
-                );
-            }
-        });
-        resolve(
-            `Test passed for an array of random numbers between ${max} and ${min}`
+  return new Promise((resolve, reject) => {
+    values.forEach((number) => {
+      if (!values.includes(number)) {
+        reject(
+          `ERROR: array of random numbers between ${max} and ${min} does not include ${number}`
         );
+      }
     });
+    resolve(
+      `Test passed for an array of random numbers between ${max} and ${min}`
+    );
+  });
 };
 // Run a test on the array occurrences to check it contains every number between max and min,
 // and return a promise
 
 checkArray()
-    .then(resolve => console.log(resolve))
-    .catch(error => console.log(error));
+  .then((resolve) => console.log(resolve))
+  .catch((error) => console.log(error));
 // Log to the console the results of checkArray
